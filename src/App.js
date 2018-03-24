58251ed218b4d229226012351c5309ab3c686270
Pedro Gomes <pedrogomes94@gmail.com>
2018-03-24 13:49:14 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
-  }
+  state = {
+    loading: false,
+    response: '',
+    input: '',
+    timeelapsed: ''
+  };
 
-  handleClick = (e) => {
+  handleClick = e => {
     e.preventDefault();
-
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
+    if (this.state.input.length < 42) {
+      this.setState({ loading: false, response: 'Address invalid' });
+      return;
+    }
+    this.setState({ loading: true, response: '' });
+    const now = Date.now();
+    axios.get(`/.netlify/functions/hello/${this.state.input}`).then(({ data }) => {
+      console.log(data);
+      this.setState({
+        loading: false,
+        response: `Balance: ${data} ETH`,
+        timeelapsed: Date.now() - now
+      });
+    });
+  };
 
   render() {
-    const {loading, msg} = this.state;
+    const { loading, response } = this.state;
 
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
+    return (
+      <div>
+        <p>Type your address</p>
+        <input
+          style={{
+            border: '1px solid lightgrey',
+            width: '300px',
+            padding: '8px',
+            'border-radius': '8px'
+          }}
+          value={this.state.input}
+          onChange={({ target }) => this.setState({ input: target.value })}
+        />
+        <br />
+        <button
+          style={{
+            background: 'grey',
+            color: 'white',
+            margin: '12px',
+            padding: '8px',
+            'border-radius': '8px'
+          }}
+          onClick={this.handleClick}
+        >
+          {loading ? 'Loading...' : 'Submit'}
+        </button>
+        <br />
+
+        <p>{response}</p>
+        <br />
+        {/* <p>{`${this.state.timeelapsed ? this.state.timeelapsed} ms`}</p> */}
+      </div>
+    );
   }
 }
 
@@ -33,12 +74,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">ETH Balance</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LambdaDemo/>
+        <LambdaDemo />
       </div>
     );
   }
